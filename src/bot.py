066bcfdd5469966ee34386d7672d1cd46662860f
Leo <leonel09-@hotmail.com>
2026-05-29 00:40:35 -0300

    refactor - nuevo mensaje y nuevos metodos

--- src/bot.py
+++ src/bot.py
@@ -2,13 +2,12 @@ from ppi_client.ppi import PPI
 from datetime import datetime
 from dotenv import load_dotenv
 from os import getenv
-from src.config import HUSO_HORARIO_ARG
 
 try: 
-    from .config import configurar_logging
+    from .config import configurar_logging, HUSO_HORARIO_ARG, CIERRE_MERVAL, APERTURA_MERVAL
     from .database import guardar_tasas
 except ImportError:
-    from config import configurar_logging
+    from config import configurar_logging, HUSO_HORARIO_ARG, CIERRE_MERVAL, APERTURA_MERVAL
     from database import guardar_tasas
 
 #----------------------------------------
@@ -20,14 +19,28 @@ load_dotenv()
 public_key=getenv('PPI_PUBLIC_KEY')
 private_key=getenv('PPI_SECRET_KEY')
 
+logger = configurar_logging()
+
 ppi = None
 
-def conectar_ppi():
+#----------------------------------------
+# Métodos PRIVADOS
+#----------------------------------------
+
+def _conectar_ppi():
     global ppi
     ppi = PPI(sandbox=False)
     ppi.account.login_api(public_key, private_key)
 
-logger = configurar_logging()
+    logger.info("Conectado a PPI exitosamente")
+
+def _es_fin_de_semana() -> bool:
+    hoy = datetime.now(HUSO_HORARIO_ARG).weekday()
+
+    if hoy >= 5:
+        return True
+    else: 
+        return False
 
 #----------------------------------------
 # GETTERS y SETTERS
@@ -37,6 +50,9 @@ def get_tasas_caucion() -> dict:
     """Obtener tasas y volumen de cauciones (24h, 48h, 72h, 168h)"""
 
     tasas = {}
+
+    if ppi is None:
+        _conectar_ppi()
 
     try:
 
@@ -68,3 +84,111 @@ def get_tasas_caucion() -> dict:
 # Métodos PUBLICOS
 #----------------------------------------
 
+def mercado_abierto() -> bool:
+    """Verificar si el MERVAL está abierto"""
+   
+    ahora = datetime.now(HUSO_HORARIO_ARG).time()
+
+    if _es_fin_de_semana():
+        return False
+    elif ahora <= APERTURA_MERVAL:
+        return False
+    elif ahora >= CIERRE_MERVAL:
+        return False
+    else:
+        return True
+    
+# No me gusta como quedo, hay que revisar
+def calcular_cambios_en_tasas(tasas_anteriores: dict, tasas_nuevas:dict) -> dict:
+    """Calcular cambios entre tasas antiguas y nuevas"""
+    
+    if not tasas_anteriores or not tasas_nuevas:
+        return None
+
+    cambios = {}
+
+    for periodo in ['1d', '2d', '3d', '7d']:
+        valor_anterior = tasas_anteriores.get(periodo, 0)
+        valor_nuevo = tasas_nuevas.get(periodo, 0)
+        
+        if valor_anterior == 0:
+            cambios[periodo] = {
+                'absolute': 0,
+                'percentage': 0,
+                'changed': False
+            }
+        else:
+            variacion_absoluta = valor_nuevo - valor_anterior
+            variacion_porcentual = (variacion_absoluta / valor_anterior) * 100
+
+            cambios[periodo] = {
+                'old': valor_anterior,
+                'new': valor_nuevo,
+                'absolute': variacion_absoluta,
+                'percentage': variacion_porcentual,
+                'changed': abs(variacion_absoluta) > 0.001  # Tolerancia para floats
+            }
+
+    return cambios
+
+#Trabajar mensaje, no me convence el formato actual
+def mostrar_tasas(tasas: dict, mercado_cerrado: bool, cambios: dict = None) -> str:
+    '''Dar formato a mensaje de tasas'''
+    if not tasas:
+        return "❌ Error al obtener las tasas de cauciones"
+
+    if mercado_cerrado:
+        mensaje = "🔒 *MERCADO CERRADO*\n\n📊 *Últimas tasas registradas:*\n\n"
+    else:
+        mensaje = "📊 *TASAS DE CAUCIONES*\n\n"
+
+    for periodo in [('1d'), ('2d'), ('3d'), ('7d')]:
+        rate = tasas[periodo]
+        volumen = tasas[f'volumen_{periodo}']
+        mensaje += f"🕐 {periodo.upper()}: {rate}% TNA"
+
+        if cambios and periodo in cambios and cambios[periodo]['changed']:
+            cambio = cambios[periodo]
+
+            if cambio['absolute'] > 0:
+                flecha = "📈"
+                signo = "+"
+            else:
+                flecha = "📉"
+                signo = ""
+            
+            mensaje += f" {flecha} {signo}{cambio['absolute']:.2f}%"
+        
+        mensaje += f'\n💰 Volumen: {volumen}\n'
+        mensaje += "\n"
+
+    mensaje += f"🗓️  Actualizado: {tasas['timestamp']}"
+
+    if not mercado_cerrado:
+        mensaje += "\n\n📅 *Horario del mercado:* Lun-Vie 10:30 - 17:00"
+
+    return mensaje
+
+
+datos = get_tasas_caucion()
+print(datos)
+
+tasas_antes = {'1d': 80.1, 'volumen_1d': '4.993.156.072.351',
+     '2d': 17.5, 'volumen_2d': '0',
+     '3d': 19.0, 'volumen_3d': '0',
+     '7d': 18.3, 'volumen_7d': '86.343.528.013',
+     'timestamp': '2026-05-28 23:04:55'
+}
+
+tasas_ahora = {'1d': 30.1, 'volumen_1d': '4.993.156.072.351',
+     '2d': 27.5, 'volumen_2d': '0',
+     '3d': 29.0, 'volumen_3d': '0',
+     '7d': 38.3, 'volumen_7d': '86.343.528.013',
+     'timestamp': '2026-05-28 23:04:55'
+}
+
+test_cambios = calcular_cambios_en_tasas(tasas_antes, tasas_ahora)
+print(test_cambios)
+
+mensaje_tasas = mostrar_tasas(tasas_ahora, True, test_cambios)
+print(mensaje_tasas)
